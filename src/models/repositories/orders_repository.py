1ec7789bd2a9de2171874c01b3e0302220d733a9
Davi Oliveira <davioliveiraes7@gmail.com>
2026-05-30 09:07:17 -0300

    feat: Implementing select actions

--- src/models/repositories/orders_repository.py
+++ src/models/repositories/orders_repository.py
@@ -1,3 +1,5 @@
+from bson.objectid import ObjectId
+
 class OrdersRepository:
     def __init__(self, db_connection) -> None:
         self.__collection_name = "orders"
@@ -10,3 +12,28 @@ class OrdersRepository:
     def insert_list_of_documents(self, list_of_documents: list) -> None:
         collection = self.__db_connection.get_collection(self.__collection_name)
         collection.insert_many(list_of_documents)
+
+    def select_many(self, doc_find: dict) -> list:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find(doc_find)
+        return data
+
+    def select_one(self, doc_find: dict) -> dict:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        response = collection.find_one(doc_find)
+        return response
+
+    def select_many_with_properties(self, doc_find: dict, properties: dict) -> list:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find(doc_find, properties)
+        return data
+
+    def select_if_property_exists(self, property_name: str, properties: dict) -> dict:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        response = collection.find({property_name: { "$exists": True }}, properties)
+        return response
+
+    def select_by_object_id(self, object_id: str) -> dict:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find_one({"_id": ObjectId(object_id)})
+        return data
